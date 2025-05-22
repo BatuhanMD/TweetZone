220dd9ea3019b621fb50b07b9102206e08d20cb7
Batuhan Mikail Demir <batuhand960@gmail.com>
2025-05-23 00:58:08 +0300

    List/Add Tweet pages changed , time info added , some css changes

--- tweetapp/models.py
+++ tweetapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone   
 # Create your models here.
 
 class Tweet(models.Model):
     username = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     message = models.CharField(max_length=255)
-
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"Username: {self.username}, Tweet: {self.message}"
